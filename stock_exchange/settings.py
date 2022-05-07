003e9a2415d0289ecf876b9f403d42dc34638a28
Aleksandr <aleksandr.gerasimov.05@gmail.com>
2022-05-07 18:21:21 +0700

    Added password reset with email, templates + logic

--- stock_exchange/settings.py
+++ stock_exchange/settings.py
@@ -84,6 +84,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# Email password recovery
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = BASE_DIR / 'sent_email'
+
 
 # Internationalization
 
