1e21b82a1ae97fe1d64853991c55604edd5b9cf4
Aleksandr <aleksandr.gerasimov.05@gmail.com>
2022-05-23 19:41:47 +0700

    Moved IndustryManager and CompanyManager to managers.py

--- companies/models.py
+++ companies/models.py
@@ -1,20 +1,11 @@
 from django.db import models
-from django.db.models import Prefetch, Sum
 from django.utils.safestring import mark_safe
 from sorl.thumbnail import get_thumbnail
 from tinymce.models import HTMLField
 
+from companies.managers import *
 from rating.models import Rating
 from users.models import CustomUser
-
-
-class IndustryManager(models.Manager):
-    def get_companies_by_industry(self):
-        companies_by_industry = dict()
-        for industry in self.get_queryset().filter(companies__is_active=True).all():
-            if len(industry.companies.all()) > 0:
-                companies_by_industry[industry] = industry.companies.all()
-        return companies_by_industry
 
 
 class Industry(models.Model):
@@ -28,19 +19,6 @@ class Industry(models.Model):
     class Meta:
         verbose_name = 'Отрасль'
         verbose_name_plural = 'Отрасли'
-
-
-class CompanyManager(models.Manager):
-    def get_sorted_companies_by_industry(self):
-        return (
-            self.get_queryset()
-                .filter(is_active=True)
-                .select_related('industry')
-                .prefetch_related(Prefetch('rating', queryset=Rating.rating.all()))
-                .only('name', 'industry__name', 'upload')
-                .annotate(sum_points=Sum('rating__points'))
-                .order_by('industry__name', '-sum_points')
-        )
 
 
 class Company(models.Model):
